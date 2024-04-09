73d6cb00e8d5bb4ccd4b8082d1dc95bd899bc26d
ayoshinin <ayodejisamson73@yahoo.com>
2024-04-09 14:16:35 -0700

    script close

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,12 @@
+
+#!/usr/bin/node
+/**
+ * returns the reverse list
+ * @param {Array} list - the list to be reversed
+ */
+exports.esrever = function (list) {
+  const reversedList = [];
+  list.forEach(item => reversedList.unshift(item));
+  return reversedList;
+};
+
