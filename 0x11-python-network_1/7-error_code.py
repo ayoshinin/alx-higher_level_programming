8140166c30f09f5abd15ce6f9295b6a843034ebc
ayoshinin <ayodejisamson73@yahoo.com>
2024-04-26 07:29:30 -0700

    restapi

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,18 +1,20 @@
 
 #!/usr/bin/python3
-""" Takes in a URL and sends a request to the URL
-and displays the body of the response
+"""A script that
+- takes in a URL
+- sends a request to the URL
+- displays the body of the response.
 """
-
 import sys
 import requests
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
 
-    response = requests.get(url)
-    if response.status_code >= 400:
-        print("Error code: {}".format(response.status_code))
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
     else:
-        print(response.text)
+        print(r.text)
 
