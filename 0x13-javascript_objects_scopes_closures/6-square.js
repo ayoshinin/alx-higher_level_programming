114d646881ad65d763e3fd9502abc072a5781ef5
ayoshinin <ayodejisamson73@yahoo.com>
2024-06-20 05:07:05 -0700

    script closure

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,22 +1,19 @@
-
 #!/usr/bin/node
-const FirstSquare = require('./5-square');
-/**
- * class Square - that defines a square and inherits from Rectangle of 4-rectangle.js
- */
-class Square extends FirstSquare {
-  /**
-   * charPrint() - Prints Square with the specified character
-   * @param {String} c The specified character.
-   */
+const SquareP = require('./5-square');
+
+class Square extends SquareP {
   charPrint (c) {
-    c = c === undefined ? 'X' : c;
-    for (let i = 0; i < this.width; i++) {
-      let square = '';
-      for (let j = 0; j < this.height; j++) square += c;
-      console.log(square);
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += c;
+      }
+      console.log(s);
     }
   }
 }
-module.exports = Square;
 
+module.exports = Square;
