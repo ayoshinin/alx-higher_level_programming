193f4f96a64de191f9b53b895762f20035bb08ad
ayoshinin <ayodejisamson73@yahoo.com>
2024-07-05 05:04:28 -0700

    file bug

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+
+#!/usr/bin/python3
+'''
+Write a Python script that fetches https://alx-intranet.hbtn.io/status and must use the package urllib
+'''
+import urllib.request as reqobj
+
+
+if __name__ == "__main__":
+    with reqobj.urlopen('https://alx-intranet.hbtn.io/status/') as response:
+        the_page = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(the_page)))
+        print("\t- content: {}".format(the_page))
+        print("\t- utf8 content: {}".format(the_page.decode("utf-8")))
+
