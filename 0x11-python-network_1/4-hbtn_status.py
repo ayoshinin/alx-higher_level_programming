419f010f8b4fb560e8230adf716c53553fff3408
ayoshinin <ayodejisamson73@yahoo.com>
2024-07-05 08:18:12 -0700

    network pair

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,15 @@
+
+#!/usr/bin/python3
+'''
+Script that fetches https://alx-intranet.hbtn.io/status
+'''
+import requests
+
+
+if __name__ == "__main__":
+    """ fetches http://0.0.0.0:5050/status"""
+    res = requests.get('https://intranet.hbtn.io/status')
+    print('Body response:')
+    print('\t- type: {}'.format(type(res.text)))
+    print('\t- content: {}'.format(res.text))
+
